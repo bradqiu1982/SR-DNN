54439c37169215a83eec681665138e1a98a9f9f7
bradqiu1982 <bradqiu1982@163.com>
2021-03-31 21:47:25 +0800

    add frozen pb converter into image classify

--- TSFLOW/imgclassify.py
+++ TSFLOW/imgclassify.py
@@ -11,13 +11,15 @@ import tensorflow_hub as hub
 import numpy as np
 from tensorflow import keras
 
+from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
+
 SZ=454
 #SZ = 224
 #FILEFOLDER = '\\\\wux-engsys01\\PlanningForCast\\VMI'
 FILEFOLDER = '\\\\wux-engsys01\\PlanningForCast\\flowers'
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 BATCH_SIZE = 32
-
+os.environ["TFHUB_CACHE_DIR"] = "./hub_model"
 
 
 def preprocess_image(image_raw):
@@ -117,7 +119,6 @@ def train_by_self():
 
 
 def train_by_mobilev3_hub():
-	os.environ["TFHUB_CACHE_DIR"] = "./hub_model"
 	ds,image_count,classcnt = get_training_ds()
 	keras_ds = ds.map(change_range)
 	model = tf.keras.models.Sequential()
@@ -127,7 +128,7 @@ def train_by_mobilev3_hub():
 	model.add(tf.keras.layers.Conv2D(3, (3, 3), activation='relu'))
 	model.add(hub.KerasLayer("https://tfhub.dev/google/imagenet/mobilenet_v3_large_100_224/feature_vector/5", trainable=True))
 	model.add(tf.keras.layers.Dropout(rate=0.2))
-	model.add(tf.keras.layers.Dense(classcnt, kernel_regularizer=tf.keras.regularizers.l2(0.0001)))
+	model.add(tf.keras.layers.Dense(classcnt, activation='softmax'))
 	model.trainable=True
 
 	model.compile(optimizer=tf.keras.optimizers.SGD(lr=0.005, momentum=0.9), loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=["sparse_categorical_accuracy"])
@@ -158,7 +159,6 @@ def train_by_vgg19():
 	steps_per_epoch=tf.math.ceil(image_count/BATCH_SIZE).numpy()
 	model.fit(ds,epochs=3,steps_per_epoch=steps_per_epoch)
 	model.save('./FLOWER_CLASS_VGG19.h5')
-	
 
 def VerifySelfModel():
 	data_root = pathlib.Path(FILEFOLDER)
@@ -182,7 +182,7 @@ def VerifySelfModel():
 	mxidx = np.argmax(res.flatten())
 	print(label_to_index[mxidx])
 
-def VerifyVGG19Model():
+def VerifyModels(tp):
 	data_root = pathlib.Path(FILEFOLDER)
 	all_image_paths = list(data_root.glob('*/*'))
 	all_image_paths = [str(path) for path in all_image_paths]
@@ -195,17 +195,66 @@ def VerifyVGG19Model():
 	image_tensor  = tf.image.resize(image_tensor , [SZ, SZ])
 	#image_tensor = tf.cast(image_tensor,tf.float32)
 	image_tensor  /= 255.0
-	#image_tensor = tf.cast(image_tensor,tf.uint8);
-	image_tensor = tf.expand_dims(image_tensor, axis=0)
 
-	model = tf.keras.models.load_model('./FLOWER_CLASS_VGG19.h5')
+	#image_tensor = tf.cast(image_tensor,tf.uint8);
+
+
+	model = ''
+	if tp == 'VGG19':
+		print('VGG19 MODEL')
+		model = tf.keras.models.load_model('./FLOWER_CLASS_VGG19.h5')
+	elif tp == 'MBV3':
+		print('MOBILEV3 MODEL')
+		image_tensor = 2*image_tensor - 1
+		model = tf.keras.models.load_model('./FLOWER_CLASS_mobilev3.h5', custom_objects={'KerasLayer': hub.KerasLayer})
+	else:
+		print('SELF MODEL')
+		model = tf.keras.models.load_model('./VCSEL_CLASS_self.h5')
+
+	image_tensor = tf.expand_dims(image_tensor, axis=0)
 	res = model.predict(image_tensor)
 	print(res.flatten())
 	mxidx = np.argmax(res.flatten())
 	print(label_to_index[mxidx])
 
+def convertmodeltopb(oldmodelfn,newmodelfn):
+	model = tf.keras.models.load_model(oldmodelfn, custom_objects={'KerasLayer': hub.KerasLayer})
+	#path of the directory where you want to save your model
+	frozen_out_path = './'
+	# name of the .pb file
+	frozen_graph_filename = newmodelfn
+	full_model = tf.function(lambda x: model(x))
+
+	full_model = full_model.get_concrete_function(tf.TensorSpec(model.inputs[0].shape, model.inputs[0].dtype))
+
+	# Get frozen ConcreteFunction
+	frozen_func = convert_variables_to_constants_v2(full_model)
+	frozen_func.graph.as_graph_def()
+	layers = [op.name for op in frozen_func.graph.get_operations()]
+
+	print("-" * 60)
+	print("Frozen model layers: ")
+	for layer in layers:
+	    print(layer)
+
+	print("-" * 60)
+	print("Frozen model inputs: ")
+	print(frozen_func.inputs)
+	print("Frozen model outputs: ")
+	print(frozen_func.outputs)
+
+	# Save frozen graph to disk
+	tf.io.write_graph(graph_or_graph_def=frozen_func.graph,
+	                  logdir=frozen_out_path,
+	                  name=f"{frozen_graph_filename}.pb",
+	                  as_text=False)
 
 #train_by_self()
 #VerifySelfModel()
 #train_by_vgg19()
-train_by_mobilev3_hub()
+#train_by_mobilev3_hub()
+#'VGG19','MBV3'
+VerifyModels('MBV3')
+
+#convertmodeltopb('./FLOWER_CLASS_mobilev3.h5','FLOWER_CLASS_mobilev3pb')
+
