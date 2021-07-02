11c3020450a0db6750dc41ff9d70e207eada7aca
bradqiu1982 <bradqiu1982@163.com>
2021-07-02 16:51:31 +0800

    Update xml_to_csv.py
    make the column match tfrecord dataset

--- TSFLOW/xml_to_csv.py
+++ TSFLOW/xml_to_csv.py
@@ -10,25 +10,25 @@ def xml_to_csv(path):
         tree = ET.parse(xml_file)
         root = tree.getroot()
         for member in root.findall('object'):
-            value = (root.find('filename').text,
+            value = (int(root.find('size')[1].text),
                      int(root.find('size')[0].text),
-                     int(root.find('size')[1].text),
-                     member[0].text,
                      int(member[4][0].text),
-                     int(member[4][1].text),
                      int(member[4][2].text),
-                     int(member[4][3].text)
+                     int(member[4][1].text),
+                     int(member[4][3].text),
+                     root.find('filename').text,
+                     member[0].text
                      )
             xml_list.append(value)
-    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    column_name = [ 'height','width', 'xmin', 'xmax', 'ymin', 'ymax', 'filename', 'class']
     xml_df = pd.DataFrame(xml_list, columns=column_name)
     return xml_df
 
 
 def main():
-    image_path = os.path.join(os.getcwd(), 'SRCIMG')
+    image_path = os.path.join(os.getcwd(), 'PADVRF')
     xml_df = xml_to_csv(image_path)
-    xml_df.to_csv('SRCIMG.csv', index=None)
+    xml_df.to_csv('PADVRF.csv', index=None)
     print('Successfully converted xml to csv.')
 
 
