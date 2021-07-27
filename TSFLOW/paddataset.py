3a8d9eec992e1ef4ddcd484014c8d9f78d6244ff
bradqiu1982 <bradqiu1982@163.com>
2021-07-27 13:48:48 +0800

    add solution for special case

--- TSFLOW/paddataset.py
+++ TSFLOW/paddataset.py
@@ -120,7 +120,7 @@ def drawtangle(box,cimg,high,width):
 		cv2.rectangle(cimg,(pxmin,pymin),(pxmax,pymax),(0,255,0),3)
 
 def verifypad():
-    model = tf.saved_model.load('./tfrec/orion_savedmodel/saved_model')
+    model = tf.saved_model.load('./tfrec/orion_savedmodel_insp/saved_model')
 
     data_root = pathlib.Path('D:/PlanningForCast/condaenv/PAD2/shortcheck2')
     all_image_paths = list(data_root.glob('*'))
@@ -137,7 +137,7 @@ def verifypad():
             # print(output_dict['detection_classes'], file=open("./res_cla.txt", "a"))
             #print(output_dict['detection_scores'], file=open("./res_score.txt", "a"))
             # print(output_dict['detection_multiclass_scores'], file=open("./res_mulscore.txt", "a"))
-
+            print(fn)
             cimg = cv2.imread(fn,cv2.IMREAD_COLOR)
             cimg = cv2.resize(cimg,(1024,1024))
             high,width,CH = cimg.shape
@@ -147,14 +147,15 @@ def verifypad():
             output_dict = model(img_tensor)
 
             for i in range(100):
-                if float(output_dict['detection_scores'][0][i]) > 0.05:
+                if float(output_dict['detection_scores'][0][i]) >= 0.03:
                     drawtangle(output_dict['detection_boxes'][0][i],cimg,high,width)
             cv2.imwrite(fn.replace('.jpg','_detect.jpg'),cimg)
 
 
 
 def getdetectboxes(fn,model):
-	boxes = []
+	boxes005 = []
+	boxes01 = []
 	cimg = cv2.imread(fn,cv2.IMREAD_COLOR)
 	cimg = cv2.resize(cimg,(1024,1024))
 	#high,width,CH = cimg.shape
@@ -164,9 +165,11 @@ def getdetectboxes(fn,model):
 	output_dict = model(img_tensor)
 
 	for i in range(100):
-		if float(output_dict['detection_scores'][0][i]) > 0.05:
-			boxes.append(output_dict['detection_boxes'][0][i])
-	return boxes
+		if float(output_dict['detection_scores'][0][i]) >= 0.05:
+			boxes005.append(output_dict['detection_boxes'][0][i])
+		if float(output_dict['detection_scores'][0][i]) >= 0.1:
+			boxes01.append(output_dict['detection_boxes'][0][i])
+	return boxes005,boxes01
 
 def getfilterboxes(boxes):
 	x = 0
@@ -229,12 +232,29 @@ def compareboxes_(beforeboxes,afterboxes):
 		if(matched == 0):
 			newbox.append(abox)
 
+	if len(newbox) > 1:
+		print('newbox has more box.................')
+		mindist = 1.0
+		matchidx = 0
+		idx = 0
+		for bx in newbox:
+			if abs(bx[0] - 0.5) < mindist:
+				matchidx = idx
+				mindist = abs(bx[0] - 0.5)
+			idx = idx + 1
+		retbx = []
+		retbx.append(newbox[matchidx])
+		return retbx
+
 	return newbox
 
 
-def compareboxes(beforeboxes,afterboxes):
+def compareboxes(beforeboxes,afterboxes,beforeboxes01,afterboxes01):
 	beforelen = len(beforeboxes)
 	afterlen = len(afterboxes)
+
+	beforelen01 = len(beforeboxes01)
+	afterlen01 = len(afterboxes01)
 
 	beforefilterbx = getfilterboxes(beforeboxes)
 	afterfilterbx = getfilterboxes(afterboxes)
@@ -250,9 +270,11 @@ def compareboxes(beforeboxes,afterboxes):
 			newbox = compareboxes_(beforefilterbx,afterfilterbx)
 		else:
 			newbox = compareboxes_(beforeboxes,afterboxes)
-	else:
-		if(afterfltlen -beforefltlen) == 1:
+	elif(afterfltlen -beforefltlen) == 1:
 			newbox = compareboxes_(beforefilterbx,afterfilterbx)
+	else:
+		if(afterlen01 - beforelen01) == 1:
+			newbox = compareboxes_(beforeboxes01,afterboxes01)
 
 	return newbox
 
@@ -425,9 +447,9 @@ def findmarkpointbox(aimg,box,fn):
 
 
 def detectnewmark():
-	model = tf.saved_model.load('./tfrec/orion_savedmodel/saved_model')
+	model = tf.saved_model.load('./tfrec/orion_savedmodel_insp/saved_model')
 
-	data_root = pathlib.Path('D:/PlanningForCast/condaenv/PAD2/shortcheck2')
+	data_root = pathlib.Path('D:/PlanningForCast/condaenv/PAD2/shortcheck1')
 	all_image_paths = list(data_root.glob('*'))
 	all_image_paths = [str(path) for path in all_image_paths]
 	for fn in all_image_paths:
@@ -437,9 +459,9 @@ def detectnewmark():
 
 			afterfn = fn.replace('before','after')
 			if pathlib.Path(afterfn).exists():
-				beforeboxes = getdetectboxes(fn,model)
-				afterboxes = getdetectboxes(afterfn,model)
-				newmarkboxes = compareboxes(beforeboxes,afterboxes)
+				beforeboxes,beforeboxes01 = getdetectboxes(fn,model)
+				afterboxes,afterboxes01 = getdetectboxes(afterfn,model)
+				newmarkboxes = compareboxes(beforeboxes,afterboxes,beforeboxes01,afterboxes01)
 
 				bimg = cv2.imread(fn,cv2.IMREAD_COLOR)
 				bimg = cv2.resize(bimg,(1024,1024))
@@ -454,8 +476,109 @@ def detectnewmark():
 				cv2.imwrite(fn.replace('.jpg','_detect.jpg'),bimg)
 				cv2.imwrite(afterfn.replace('.jpg','_detect.jpg'),aimg)
 
+				# else:
+				# 	Forspecialcase(model,bimg,aimg,fn,afterfn)
+
+
+def getimageoffset(bimg,aimg):
+	zerolist = []
+	aftersub = aimg[200:300,500:600]
+	ablurred = cv2.GaussianBlur(aftersub,(3,3),0)
+	aretval,asrcthr = cv2.threshold(ablurred,100,255,cv2.THRESH_BINARY)
+
+	# asrcthr = copy.deepcopy(ablurred)
+	# asrcthr = cv2.Canny(ablurred,50,200,asrcthr,3,False)
+	zerolist.append(10000)
+
+	matchx = -100
+	matchy = -100
+
+	for ix in range(-20,20):
+		for iy in range(-20,20):
+			beforesub = bimg[200+iy:300+iy,500+ix:600+ix]
+			bblurred = cv2.GaussianBlur(beforesub,(3,3),0)
+			bretval,bsrcthr = cv2.threshold(bblurred,100,255,cv2.THRESH_BINARY)
+			# bsrcthr = copy.deepcopy(bblurred)
+			# bsrcthr = cv2.Canny(bblurred,50,200,bsrcthr,3,False)
+			xor = cv2.bitwise_xor(bsrcthr,asrcthr)
+			nonzero = cv2.countNonZero(xor)
+			
+			if nonzero <= np.min(zerolist):
+				zerolist.append(nonzero)
+				matchx = ix
+				matchy = iy
+
+	return matchx,matchy
+
+
+def getdetectboxesImg(cimg,model):
+	boxes = []
+	cimg = cv2.resize(cimg,(1024,1024))
+
+	img_tensor = copy.deepcopy(cimg)
+	img_tensor = np.expand_dims(img_tensor, axis=0)
+	output_dict = model(img_tensor)
+
+	for i in range(100):
+		if float(output_dict['detection_scores'][0][i]) >= 0.05:
+			boxes.append(output_dict['detection_boxes'][0][i])
+	return boxes
+
+def getspecialbox(afterfn,h,w,model):
+	retbox = []
+
+	aimg = cv2.imread(afterfn,cv2.IMREAD_COLOR)
+	aimg = cv2.resize(aimg,(1024,1024))
+	cv2.rectangle(aimg,(w-4,h-4),(w+4,h+4),(255,255,255),5)
+	boxes = getdetectboxesImg(aimg,model)
+	for box in boxes:
+		ymin = box[0]
+		xmin = box[1]
+		ymax = box[2]
+		xmax = box[3]
+		pxmin = int(xmin*1024)
+		pymin = int(ymin*1024)
+		pxmax = int(xmax*1024)
+		pymax = int(ymax*1024)
+
+		if w > pxmin-10 and w < pxmax+10 and h > pymin-10 and  h < pymax+10:
+			retbox.append(pxmin)
+			retbox.append(pymin)
+			retbox.append(pxmax)
+			retbox.append(pymax)
+			return retbox
+	return retbox
+
+def Forspecialcase(model,bimg,aimg,fn,afterfn):
+	matchx,matchy = getimageoffset(bimg,aimg)
+	aftersub = aimg[20:1000,20:1000]
+	beforesub = bimg[20+matchy:1000+matchy,20+matchx:1000+matchx]
+	
+	diff = ImageChops.difference(Image.fromarray(aftersub),Image.fromarray(beforesub))
+
+	diff = np.array(diff)
+	diff = diff[200:780,200:780]
+	thr = np.max(diff)
+	res = np.where(diff == thr)
+	if len(res[0]) > 0:
+		listOfIndices= list(zip(res[0], res[1]))
+		h,w = listOfIndices[0]
+		h = h + 220
+		w = w + 220
+
+		spbox = getspecialbox(afterfn,h,w,model)
+		aimg = cv2.imread(afterfn,cv2.IMREAD_COLOR)
+		if len(spbox) > 0:
+			cv2.rectangle(aimg,(spbox[0],spbox[1]),(spbox[2],spbox[3]),(0,255,0),3)
+			cv2.rectangle(bimg,(spbox[0],spbox[1]),(spbox[2],spbox[3]),(0,255,0),3)
+
+		cv2.imwrite(fn.replace('.jpg','_detect.jpg'),bimg)
+		cv2.rectangle(aimg,(w-3,h-3),(w+3,h+3),(0,0,255),2)
+		cv2.imwrite(afterfn.replace('.jpg','_detect.jpg'),aimg)
+
 
 def specialcase():
+	model = tf.saved_model.load('./tfrec/orion_savedmodel/saved_model')
 
 	data_root = pathlib.Path('D:/PlanningForCast/condaenv/PAD2/shortcheck2')
 	all_image_paths = list(data_root.glob('*'))
@@ -473,70 +596,41 @@ def specialcase():
 				aimg = cv2.imread(afterfn,cv2.IMREAD_GRAYSCALE)
 				aimg = cv2.resize(aimg,(1024,1024))
 
-				zerolist = []
-				aftersub = aimg[200:300,500:600]
-				ablurred = cv2.GaussianBlur(aftersub,(3,3),0)
-				aretval,asrcthr = cv2.threshold(ablurred,100,255,cv2.THRESH_BINARY)
-
-				# asrcthr = copy.deepcopy(ablurred)
-				# asrcthr = cv2.Canny(ablurred,50,200,asrcthr,3,False)
-
-				zerolist.append(10000)
-
-				matchx = -100
-				matchy = -100
-
-				for ix in range(-20,20):
-					for iy in range(-20,20):
-						beforesub = bimg[200+iy:300+iy,500+ix:600+ix]
-						bblurred = cv2.GaussianBlur(beforesub,(3,3),0)
-						bretval,bsrcthr = cv2.threshold(bblurred,100,255,cv2.THRESH_BINARY)
-
-						# bsrcthr = copy.deepcopy(bblurred)
-						# bsrcthr = cv2.Canny(bblurred,50,200,bsrcthr,3,False)
-
-						xor = cv2.bitwise_xor(bsrcthr,asrcthr)
-						nonzero = cv2.countNonZero(xor)
+				matchx,matchy = getimageoffset(bimg,aimg)
 						
-						if nonzero <= np.min(zerolist):
-							zerolist.append(nonzero)
-							matchx = ix
-							matchy = iy
-						
-							#cv2.imwrite(afterfn.replace('.jpg','_xor.jpg'),xor)
-							#cv2.imwrite(afterfn.replace('.jpg','_aftersub.jpg'),asrcthr)
-							#cv2.imwrite(afterfn.replace('.jpg','_beforesub.jpg'),bsrcthr)
+				# print(fn + '  matchx: '+ str(matchx) + '  matchy: '+str(matchy))
+				# print('min nonzero:' +str(np.min(zerolist)))
 
-				print(fn + '  matchx: '+ str(matchx) + '  matchy: '+str(matchy))
-				print('min nonzero:' +str(np.min(zerolist)))
-
-				aftersub = aimg[10:1000,10:1000]
-				beforesub = bimg[10+matchy:1000+matchy,10+matchx:1000+matchx]
+				aftersub = aimg[20:1000,20:1000]
+				beforesub = bimg[20+matchy:1000+matchy,20+matchx:1000+matchx]
 				
 				diff = ImageChops.difference(Image.fromarray(aftersub),Image.fromarray(beforesub))
 
 				diff = np.array(diff)
-				diff = diff[200:790,200:790]
+				diff = diff[200:780,200:780]
 				thr = np.max(diff)
 				res = np.where(diff == thr)
 				if len(res[0]) > 0:
 					listOfIndices= list(zip(res[0], res[1]))
-					# xorret,diff = cv2.threshold(diff,thr-10,255,cv2.THRESH_BINARY)
 					h,w = listOfIndices[0]
-					h = h + 210
-					w = w + 210
+					h = h + 220
+					w = w + 220
+
+					spbox = getspecialbox(afterfn,h,w,model)
 					aimg = cv2.imread(afterfn,cv2.IMREAD_COLOR)
+					if len(spbox) > 0:
+						cv2.rectangle(aimg,(spbox[0],spbox[1]),(spbox[2],spbox[3]),(0,255,0),3)
 					cv2.rectangle(aimg,(w-3,h-3),(w+3,h+3),(0,0,255),2)
 					cv2.imwrite(afterfn.replace('.jpg','_lock.jpg'),aimg)
 
 
-specialcase()
+#specialcase()
 
 #detectnewmark()
 
 #convertimag2()
 
-#verifypad()
+verifypad()
 
 # writesiglefile('\\\\wux-engsys01\\PlanningForCast\\VCSEL5\\XYFILE\\OrionTrain.txt','./tfrec/Orion_train_dataset_new.tfrecord')
 
