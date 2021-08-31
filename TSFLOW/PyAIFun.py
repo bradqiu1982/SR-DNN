4cd50ee70c63ba3afd7cecb4cec653f8922bfa9f
bradqiu1982 <bradqiu1982@163.com>
2021-08-31 13:30:49 +0800

    add XRAY detector

--- TSFLOW/PyAIFun.py
+++ TSFLOW/PyAIFun.py
@@ -1,3 +1,13 @@
+
+import os
+import pathlib
+import numpy as np
+import cv2
+import copy
+import sys
+import math
+
+
 import tensorflow as tf
 from object_detection.utils import dataset_util
 
@@ -5,12 +15,7 @@ from object_detection.utils import ops as utils_ops
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
 
-import pathlib
-import numpy as np
-import cv2
-import copy
-import sys
-
+from tensorflow import keras
 from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
 from tensorflow.lite.python.util import run_graph_optimizations, get_grappler_config
 
@@ -19,15 +24,353 @@ from tensorflow.python.client import session
 import json
 from flask import *
 
+from multiprocessing import Lock
 
+mylock = Lock()
 cache = dict()
 # initialize the flask application
 app = Flask(__name__)
 
 
-def getOBJDectModel(imgtype):
+def getXRayOBJDetectModel():
+	if 'XRAY' not in cache:
+		model = tf.saved_model.load('./ObjectDetectModel/xray_savemodel_215_good/saved_model')
+		cache['XRAY'] = model
+		return model
+	else:
+		return cache['XRAY']
+
+
+@app.route("/XRAYOBJDetect", methods=["POST"])
+def XRAYOBJDetect():
+	try:
+		res = []
+		request_json = request.get_json()
+		ipth = request_json['imgpath']
+
+		despath = ipth.replace('\\SRC\\','\\DES\\')
+		if not pathlib.Path(despath).is_dir():
+			os.mkdir(despath)
+
+		model =  getXRayOBJDetectModel()
+		data_root = pathlib.Path(ipth)
+		all_image_paths = list(data_root.glob('*'))
+		all_image_paths = [str(path) for path in all_image_paths]
+		for fn in all_image_paths:
+			if '.JPG' in fn.upper() or '.JPEG' in fn.upper():
+
+				mylock.acquire()
+				try:
+					print(fn)
+					cimg = cv2.imread(fn,cv2.IMREAD_COLOR)
+					cimg = cv2.resize(cimg,(1280,920))
+					high,width,CH = cimg.shape
+					img_tensor = copy.deepcopy(cimg)
+					img_tensor = np.expand_dims(img_tensor, axis=0)
+					output_dict = model(img_tensor)
+
+					warninginfos = solveXRAYImg(fn,output_dict,cimg)
+					for item in warninginfos:
+						res.append(item)
+				except:
+					exception_message = sys.exc_info()[1]
+					print(str(exception_message))
+				finally:
+					mylock.release()
+
+		response = jsonify(res)
+		response.status_code = 200
+	except:
+		exception_message = sys.exc_info()[1]
+		print(str(exception_message))
+		response = jsonify({"content":str(exception_message)})
+		response.status_code = 400
+	return response
+
+def ImageType(gydelta,gyavg):
+	if gydelta > 100:
+		return 1
+	else:
+		if gyavg < 90:
+			return 2
+		else:
+			return 3
+
+def getRealBond(cimg,box):
+	high,width,CH = cimg.shape
+	ymin = box[0]
+	xmin = box[1]
+	ymax = box[2]
+	xmax = box[3]
+	pxmin = int(xmin*width)
+	pymin = int(ymin*high)
+	pxmax = int(xmax*width)
+	pymax = int(ymax*high)
+	
+	oleft = pxmin-4
+	oright = pxmax+4
+	otop = pymin-4
+	obotm = pymax+4
+
+	if oleft < 0:
+		oleft = 0
+	if otop < 0:
+		otop = 0
+	if oright > width:
+		oright = width-1
+	if obotm > high:
+		obotm = high-1
+
+	#print( ' oleft ' +str(oleft)+' oright ' +str(oright)+'  otop ' +str(otop)+'  obotm  ' +str(obotm))
+
+	subcimg = cimg[otop:obotm,oleft:oright]
+	srcgray = cv2.cvtColor(subcimg,cv2.COLOR_BGR2GRAY)
+	hg,wd = srcgray.shape
+
+	gymaxval = int(np.max(srcgray.flatten()))
+	gyminval = int(np.min(srcgray.flatten()))
+	gyavg = int(np.average(srcgray.flatten()))
+	gymidval = int((gymaxval+gyminval)/2)
+	gydelta = gymaxval-gyminval
+
+	#print('i: '+str(i)+'  maxval:  '+str(gymaxval)+' minval: '+str(gyminval) + ' delta: '+str(gydelta) + ' midval: '+str(gymidval)+' avgval: '+str(gyavg))
+	#cv2.imwrite('D:/PlanningForCast/condaenv/COGA-PAD/PAD/'+'PAD_'+str(i)+'.jpg',subcimg)
+
+	imgtype = ImageType(gydelta,gyavg)
+	thrd = 100
+	if imgtype == 1 or imgtype == 3:
+		thrd = gymidval
+		if thrd > gyavg:
+			thrd = gyavg
+	else:
+		if imgtype == 2:
+			#thrd = 80
+			thrd = gymidval
+			if thrd > gyavg:
+				thrd = gyavg
+		else:
+			thrd = 100
+
+	blurred = cv2.GaussianBlur(srcgray,(3,3),0)
+	retval,srcthr = cv2.threshold(blurred,thrd,255,cv2.THRESH_BINARY_INV)
+	
+	ckleft = 0
+	for x in range(0,int(wd/2),1):
+		submat = srcthr[10:hg-10,x:x+1]
+		nonzero = cv2.countNonZero(submat)
+		if nonzero > 4:
+			ckleft = x
+			break
+	ckleft = ckleft - 1
+
+	ckright = 0
+	for x in range(wd,int(wd/2),-1):
+		submat = srcthr[10:hg-10,x-1:x]
+		nonzero = cv2.countNonZero(submat)
+		if nonzero > 4:
+			ckright = x
+			break
+
+	ckright = ckright + 1
+	ckright = wd-ckright
+
+	cktop = 0
+	for y in range(0,int(hg/2),1):
+		submat = srcthr[y:y+1,10:wd-10]
+		nonzero = cv2.countNonZero(submat)
+		if nonzero > 4:
+			cktop = y
+			break
+	cktop = cktop - 1
+
+	ckbotm = 0
+	for y in range(hg,int(hg/2),-1):
+		submat = srcthr[y-1:y,10:wd-10]
+		nonzero = cv2.countNonZero(submat)
+		if nonzero > 4:
+			ckbotm = y
+			break
+
+	ckbotm = ckbotm + 1
+	ckbotm = hg-ckbotm
+
+	oleft = oleft + ckleft
+	oright = oright - ckright
+	otop = otop + cktop
+	obotm = obotm - ckbotm
+
+	if oleft < 0:
+		oleft = 0
+	if otop < 0:
+		otop = 0
+	high,width,CH = cimg.shape
+	if oright > width:
+		oright = width-1
+	if obotm > high:
+		obotm = high-1
+
+	return oleft,oright,otop,obotm,gydelta,gyavg
+
+
+def getgasarea(cimg,oleft,oright,otop,obotm,rd,lccx,lccy,imgtype):
+	if oleft < 0:
+		oleft = 0
+	if otop < 0:
+		otop = 0
+	high,width,CH = cimg.shape
+	if oright > width:
+		oright = width-1
+	if obotm > high:
+		obotm = high-1
+
+	subcimg = cimg[otop:obotm,oleft:oright]
+	srcgray = cv2.cvtColor(subcimg,cv2.COLOR_BGR2GRAY)
+	#blurred = cv2.GaussianBlur(srcgray,(3,3),0)
+	blurred = cv2.bilateralFilter(srcgray,3,75,75)
+	#blurred = cv2.medianBlur(srcgray,5)
+	
+	grayvals = []
+	yy,xx = blurred.shape
+	for x in range(xx):
+		for y in range(yy):
+			dist = math.sqrt((x-lccx)*(x-lccx)+(y-lccy)*(y-lccy))
+			if dist < float(rd)-1:
+				grayvals.append(blurred[y,x])
+
+	# np.set_printoptions(threshold=np.inf)
+	# print(blurred, file=open("./srcgray.txt", "a"))
+	# cv2.imwrite('./srcgray.jpg',blurred,[cv2.IMWRITE_JPEG_QUALITY,100])
+
+	maxval = int(max(grayvals))
+	minval = int(min(grayvals))
+	deltaval = int(maxval-minval)
+	midval = int((maxval+minval)/2)
+	avg = int(sum(grayvals)/len(grayvals))
+
+	gasval = midval
+	if gasval > avg:
+		gasval = avg
+
+	# print('  max: '+str(maxval)+'  min: '+str(minval)+' midval: '+str(int((maxval+minval)/2))+' avgval: '+str(avg)+ ' delta: '+str(maxval-minval))
+
+	if imgtype == 1 or imgtype == 3:
+		gasval += int(deltaval*0.1)
+		# gasval = 82
+		# if gasval < 82:
+		# 	gasval = 82
+	elif imgtype == 2:
+		gasval += int(deltaval*0.2)
+		# if gasval < 78:
+		# 	gasval = 78
+	else:
+		gasval += int(deltaval*0.1)
+
+	gasarea = 0
+	yy,xx = blurred.shape
+	for x in range(xx):
+		for y in range(yy):
+			val = blurred[y,x]
+			if val >= gasval:
+				dist = math.sqrt((x-lccx)*(x-lccx)+(y-lccy)*(y-lccy))
+				if dist < float(rd)-5:
+					gasarea = gasarea+1
+					cimg[otop+y,oleft+x] = [255,0,0]
+					#cv2.circle(cimg,(oleft+x,otop+y),0,(255,0,0),-1)
+	return gasarea
+
+
+def calculateRate(desfn,cimg,box,boxcount):
+
+	res = []
+
+	oleft,oright,otop,obotm,gydelta,gyavg = getRealBond(cimg,box)
+	imgtype = ImageType(gydelta,gyavg)
+
+	cx = int((oleft+oright)/2)
+	cy = int((otop+obotm)/2)
+	lccx = int((oright-oleft)/2)
+	lccy = int((obotm - otop)/2)
+	hg = obotm - otop
+	wd = oright - oleft
+
+	if boxcount > 56 and hg < 30:
+		return res
+
+	# rd = wd/2
+	# if hg > wd:
+	# 	rd = hg/2
+	# if imgtype == 2 or imgtype == 3:
+	# 	rd = wd/2
+	# 	if hg < wd:
+	# 		rd = hg/2
+
+	rd = wd/2
+	if hg < wd:
+		rd = hg/2
+
+	if rd < 8:
+		return res
+
+	# print('cx: '+ str(cx) + '  cy: '+str(cy)+'  rd: '+str(rd))
+	
+	retval = {}
+	gasarea = getgasarea(cimg,oleft,oright,otop,obotm,rd,lccx,lccy,imgtype)
+
+	solderarea = int(np.pi*rd*rd)
+	rate = gasarea/solderarea*100
+	ccolor = (0,255,0)
+	if rate >= 35.0:
+		ccolor = (0,0,255)
+		retval['level'] = 'FAIL'
+		retval['rate'] = str(rate)[0:4]
+		retval['image'] = desfn
+	elif rate > 25.0 and rate < 35.0:
+		ccolor = (0,255,255)
+		retval['level'] = 'WARNING'
+		retval['rate'] = str(rate)[0:4]
+		retval['image'] = desfn
+
+	if len(retval) > 0:
+		res.append(retval)
+
+	#print(str(i)+':  solderarea: '+str(solderarea)+'   gasarea: '+str(gasarea) + '  rate: '+str(gasarea/solderarea))
+
+	cv2.circle(cimg,(cx,cy),int(rd),ccolor,1)
+	cv2.putText(cimg,str(rate)[0:4],(cx+5,cy-20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),1)
+
+	return res
+
+
+def solveXRAYImg(fn,output_dict,cimg):
+	desfn = fn.replace('.jpg','_detect.jpg').replace('.jpeg','_detect.jpg').replace('\\SRC\\','\\DES\\')
+	res = []
+	boxcount = 0
+	for i in range(100):
+		if float(output_dict['detection_scores'][0][i]) >= 0.05:
+			boxcount = boxcount + 1
+
+	for i in range(100):
+		if float(output_dict['detection_scores'][0][i]) >= 0.05:
+			box = output_dict['detection_boxes'][0][i]
+			warninginfos = calculateRate(desfn,cimg,box,boxcount)
+			for item in warninginfos:
+					res.append(item)
+
+	cv2.imwrite(desfn,cimg)
+	return res
+
+
+
+
+
+
+
+
+
+
+def  getVCSELOBJDectModel(imgtype):
 	if imgtype not in cache:
-		model = tf.saved_model.load('./tfrec/ObjectDetectModel/'+imgtype+'/saved_model')
+		model = tf.saved_model.load('./ObjectDetectModel/'+imgtype+'/saved_model')
 		cache[imgtype] = model
 		return model
 	else:
@@ -40,7 +383,7 @@ def SingleOBJDetect():
 		request_json = request.get_json()
 		imgtype = request_json['imgtype']
 
-		model = getOBJDectModel(imgtype)
+		model =  getVCSELOBJDectModel(imgtype)
 
 		f = request_json['imgpath']
 		if '.JPG' in f.upper() or '.JPEG' in f.upper():
@@ -91,7 +434,7 @@ def OBJDetect():
 		#print(request_json['imgpath'])
 		#print(request_json['imgtype'])
 
-		model = getOBJDectModel(request_json['imgtype'])
+		model =  getVCSELOBJDectModel(request_json['imgtype'])
 		data_root = pathlib.Path(request_json['imgpath'])
 
 
@@ -129,5 +472,5 @@ def OBJDetect():
 if __name__ == "__main__":
 	from waitress import serve
 	serve(app, host="0.0.0.0", port=5000)
-#     run flask application in debug mode
-#    app.run(debug=True)
+#    run flask application in debug mode
+	# app.run(debug=True)
---
+++ TSFLOW/XRAYOBJDetector.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace SkyEye.Models
+{
+    public class XRAYItem
+    {
+        public static List<XRAYItem> Parse(string json)
+        {
+            return (List<XRAYItem>)Newtonsoft.Json.JsonConvert.DeserializeObject(json, (new List<XRAYItem>()).GetType());
+        }
+
+        public XRAYItem() {
+
+            level = "";
+            rate = "";
+            image = "";
+        }
+        public string level { set; get; }
+        public string rate { set; get; }
+        public string image { set; get; }
+        public string imgpath { get { return @"file:///" + image.Replace("\\", "/"); } }
+    }
+
+    public class XRAYOBJDetector
+    {
+        public static List<XRAYItem> XRAYOBJDect(string imgpath)
+        {
+            var ret = new List<XRAYItem>();
+            var pathobj = new
+            {
+                imgpath = imgpath
+            };
+
+            var reqstr = Newtonsoft.Json.JsonConvert.SerializeObject(pathobj);
+            var response = PythonRESTFun("http://wux-engsys01.chn.ii-vi.net:5000/XRAYOBJDetect", reqstr);
+            if (!string.IsNullOrEmpty(response))
+            {
+                ret = XRAYItem.Parse(response);
+            }
+            return ret;
+        }
+
+        private static string PythonRESTFun(string url, string reqstr)
+        {
+            string webResponse = string.Empty;
+            try
+            {
+                Uri uri = new Uri(url);
+                WebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(uri);
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+                httpWebRequest.Timeout = -1;
+                ((HttpWebRequest)httpWebRequest).ReadWriteTimeout = -1;
+
+                using (StreamWriter streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    streamWriter.Write(reqstr);
+                }
+
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                if (httpWebResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+                    {
+                        webResponse = streamReader.ReadToEnd();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return webResponse;
+        }
+    }
+}
